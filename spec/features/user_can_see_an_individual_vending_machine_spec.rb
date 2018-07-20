30a8a57e981a2593df2cbd618d053924ebeb0519
ClaireBvs <clairembeauvais@gmail.com>
2018-07-20 10:56:47 -0600

    user story 2

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -25,9 +25,23 @@ feature 'When a user visits a vending machine show page' do
     expect(page).to have_content(snack2.name)
     expect(page).to have_content(snack2.price)
   end
-=begin
-  As a user
-When I visit a specific vending machine page
-I see the name of all of the snacks associated with that vending machine along with their price
-=end
+
+  scenario 'they see average price for all snack' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack1 = Snack.create(name: 'Oreo', price: 2)
+    snack2 = Snack.create(name: 'Mars', price: 1)
+    machine_snacks = MachineSnack.create(snack_id: snack1.id, machine_id: dons.id)
+    machine_snacks1 = MachineSnack.create(snack_id: snack2.id, machine_id: dons.id)
+    
+    visit machine_path(dons)
+
+
+    expect(page).to have_content("average price : 1.5")
+
+  end
+
+  # As a user
+  # When I visit a specific vending machine page
+  # I also see an average price for all of the snacks in that machine
 end
